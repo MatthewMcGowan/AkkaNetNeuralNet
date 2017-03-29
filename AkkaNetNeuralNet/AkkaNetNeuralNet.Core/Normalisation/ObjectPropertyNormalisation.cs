c598d4153a52693ab77868a4028fab9da4aeb689
MattMcGowan <matthew.mcgowan@abide-financial.com>
2017-03-30 00:19:53 +0100

    Further refactored out decimal normalisation to generic. Perhaps a step far.

--- AkkaNetNeuralNet/AkkaNetNeuralNet.Core/Normalisation/ObjectPropertyNormalisation.cs
+++ AkkaNetNeuralNet/AkkaNetNeuralNet.Core/Normalisation/ObjectPropertyNormalisation.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using AkkaNetNeuralNet.Core.Model;
 
 namespace AkkaNetNeuralNet.Core.Normalisation
 {
-    public static class DogProfileDecimalNormalisation
+    public static class ObjectPropertyNormalisation
     {
-        public static IEnumerable<DogProfile> Normalise(this IEnumerable<DogProfile> profiles, 
-            Func<DogProfile, decimal> propertyAccessorFunc,
-            Func<DogProfile, decimal, DogProfile> propertyUpdateInFunc)
+        public static IEnumerable<T> Normalise<T>(this IEnumerable<T> profiles, 
+            Func<T, decimal> propertyAccessorFunc,
+            Func<T, decimal, T> propertyUpdateInFunc)
         {
             decimal min = profiles.Min(propertyAccessorFunc);
             decimal max = profiles.Max(propertyAccessorFunc);
@@ -18,7 +17,7 @@ namespace AkkaNetNeuralNet.Core.Normalisation
 
             return profiles.Select(x => MinMaxNormalise(x));
 
-            DogProfile MinMaxNormalise(DogProfile profile)
+            T MinMaxNormalise(T profile)
             {
                 decimal mass = (propertyAccessorFunc(profile) - min) / (max - min);
                 return propertyUpdateInFunc(profile, mass);
